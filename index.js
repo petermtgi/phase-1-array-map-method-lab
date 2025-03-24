bcaf3a53bc2bd1bf798e4af6a144b636a5d7fec2
Peter Mutungi <3sjk0krrr@gmail.com>
2025-03-24 09:25:06 +0300

    array-map-method

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+function titleCased() { 
+  return tutorials.map(title => 
+    title
+    .split(' ')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ')
+  );
+}  
+
+console.log (titleCased (tutorials));
+ 
